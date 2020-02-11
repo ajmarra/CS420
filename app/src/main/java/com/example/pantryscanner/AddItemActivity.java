7a88701b28dcd67602ad754f44fee5dff233a072
ajmarra <ajmarra125@gmail.com>
2020-02-10 21:27:29 -0500

    scanner tutorial done

--- app/src/main/java/com/example/pantryscanner/AddItemActivity.java
+++ app/src/main/java/com/example/pantryscanner/AddItemActivity.java
@@ -2,5 +2,70 @@ package com.example.pantryscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
 public class AddItemActivity extends AppCompatActivity {
+    TextView txtView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item_activity);
+
+        Button btn = findViewById(R.id.button);
+        Button openBtn = findViewById(R.id.cameraButton);
+
+        ImageView myImageView = findViewById(R.id.imgview);
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+        myImageView.setImageBitmap(myBitmap);
+
+        txtView = findViewById(R.id.txtContent);
+
+        BarcodeDetector detector =
+                new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+        if(!detector.isOperational()){
+            txtView.setText("Could not set up the detector!");
+            return;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        final SparseArray<Barcode> barcodes = detector.detect(frame);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Barcode thisCode = barcodes.valueAt(0);
+                txtView.setText(thisCode.rawValue);
+            }
+        });
+
+        openBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = getPackageManager().getLeanbackLaunchIntentForPackage("com.Camera");
+                //startActivityForResult(i,RESULT_FIRST_USER);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+
 }
