2d431e6807e5f3260b3d875d988fce808cb5c6c6
ajmarra <ajmarra125@gmail.com>
2020-02-13 23:31:11 -0500

    can switch to camera/gallery for photo

--- app/src/main/java/com/example/pantryscanner/AddItemActivity.java
+++ app/src/main/java/com/example/pantryscanner/AddItemActivity.java
@@ -2,10 +2,14 @@ package com.example.pantryscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +21,7 @@ import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class AddItemActivity extends AppCompatActivity {
     TextView txtView;
+    ImageView myImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +29,11 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.add_item_activity);
 
         Button btn = findViewById(R.id.button);
-        Button openBtn = findViewById(R.id.cameraButton);
+        Button openBtn = findViewById(R.id.uploadButton);
+        Button cameraBtn = findViewById(R.id.cameraButton);
 
-        ImageView myImageView = findViewById(R.id.imgview);
-        Bitmap myBitmap = BitmapFactory.decodeResource(
+        myImageView = findViewById(R.id.imgview);
+        /*Bitmap myBitmap = BitmapFactory.decodeResource(
                 getApplicationContext().getResources(),
                 R.drawable.puppy);
         myImageView.setImageBitmap(myBitmap);
@@ -52,18 +58,65 @@ public class AddItemActivity extends AppCompatActivity {
                 Barcode thisCode = barcodes.valueAt(0);
                 txtView.setText(thisCode.rawValue);
             }
-        });
+        });*/
 
         openBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = getPackageManager().getLeanbackLaunchIntentForPackage("com.Camera");
-                //startActivityForResult(i,RESULT_FIRST_USER);
-                startActivity(i);
+                uploadImage();
+            }
+        });
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                switchToCamera();
             }
         });
 
+    }
 
+    private void uploadImage() {
+        Intent choosePhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(choosePhoto, 1);
+    }
+
+    private void switchToCamera() {
+        Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        myImageView.setImageBitmap(selectedImage);
+                    }
+
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage,
+                                    filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                myImageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
     }
 
 
